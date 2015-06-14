53cb40e69c864281625e11d4fb25d25ba60251fa
Vivek <vivekaxl@gmail.com>
2015-06-14 14:07:44 -0400

    the R code is ready

--- R_Code/Test.R
+++ R_Code/Test.R
@@ -1,40 +1,3 @@
-process_result <- function(listoflist){
-  N <- length(listoflist)
-  M <- length(listoflist[[1]])
-  return_value <- list(rep(0,M))
-  for(i in 1:M){
-    temp_sum <- 0
-    for(j in 1:N){
-      temp_sum <- temp_sum + listoflist[[j]][i]
-    }
-    return_value[[1]][i] <- temp_sum / N #this looks weird
-  }
-  return(return_value)
-}
-
-# test_process_result <- function(){
-#   
-#   temp <- list()
-#   expected_result <- list(rep(2.5, 4))
-#   temp[1] <-  list(c(1, 1, 1, 1))
-#   temp[2] <-  list(c(2, 2, 2, 2))
-#   temp[3] <-  list(c(3, 3, 3, 3))
-#   temp[4] <-  list(c(4, 4, 4, 4))
-#   
-#   actual_result <- process_result(temp)
-#   print(actual_result)
-#   print(expected_result)
-#   
-#   if(identical(actual_result, expected_result) == TRUE){
-#     print("test_process_result passed")
-#   }
-#   else{
-#     print("test_process_result failed")
-#   }
-# }
-# test_process_result()
-
-#-------------------------------------From http://tinyurl.com/TH-RF-Example--------------------------------#
 #source("ML10_RandomForest_UsageExample_iris.r")
 data(iris)
 
@@ -43,47 +6,30 @@ run_randomForest <- function(indep,dep){
   my.randomForest.predict <- predict(my.randomForest, indep)
   
   len.randomForest.predict <- length(my.randomForest.predict)
-  
-  return_list <- list()
-  true.positive <- 0
-  true.negative <- 0
-  false.positive <- 0
-  false.negative <- 0
+  my.randomForest.result <- vector()
+  my.randomForest.matches <- 0
   for ( i in 1:len.randomForest.predict ){
-    if ( my.randomForest.predict[i] == 1 && my.randomForest.predict[i] == dep[i]){
-      true.positive <- true.positive + 1
-    }
-    else if(my.randomForest.predict[i] == 0 && my.randomForest.predict[i] == dep[i]){
-      true.negative <- true.negative + 1
-    }
-    else if(my.randomForest.predict[i] == 1 && my.randomForest.predict[i] != dep[i]){
-      false.positive <- false.positive + 1
-    }
-    else if(my.randomForest.predict[i] == 0 && my.randomForest.predict[i] != dep[i]){
-      false.negative <- false.negative + 1
+    if ( my.randomForest.predict[i] == dep[i] ){
+      my.randomForest.matches <- my.randomForest.matches + 1
     }
   }
   
-  return_list[length(return_list)+1] <- true.positive/len.randomForest.predict
-  return_list[length(return_list)+1] <- true.negative/len.randomForest.predict
-  return_list[length(return_list)+1] <- false.positive/len.randomForest.predict
-  return_list[length(return_list)+1] <- false.negative/len.randomForest.predict
-  
-  
-  return(return_list)
+  my.randomForest.matches/len.randomForest.predict
 }
 
 multiple_runs <- function(){
-  proportion_correct <-list()
-  for( i in 1:10 ){
-    proportion_correct[i] <- run_randomForest(iris[,1:4],iris[,5])
+  repeats <- 7
+  average_correct <- 0;
+  golf <- read.csv("Golf.csv")
+  proportion_correct <-vector()
+  for( i in 1:repeats){
+    proportion_correct[i] <- run_randomForest(golf[,1:6],golf[,7])
+    average_correct <- average_correct + proportion_correct[i]
   }
   
-  average.confusion.matrix <- process_result(proportion_correct)
-  #print(average.confusion.matrix)
-  
-  
+  a <- round((average_correct/repeats)*100,digits=1)
+  x <- paste("Average correct over ", repeats, " runs is ",a,"%", sep="");
+  print(x);
 }
 
 multiple_runs()
-#-----------------------------------------------------------------------------------------------------#
